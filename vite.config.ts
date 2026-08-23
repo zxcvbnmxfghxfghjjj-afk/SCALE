fe36d90fdf5980e64fbecccf1b9f52ae35054bd1
Shimaa youssef <mwshaywsf@gmail.com>
2026-08-23 20:27:28 +0300

    Scale pro

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    base: '/SCALE/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),

ab90fa347df6ca818ebb974b19236c0e10ffb6d3
Shimaa youssef <mwshaywsf@gmail.com>
2026-08-23 20:39:27 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base: '/SCALE/',
+    base: './',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
